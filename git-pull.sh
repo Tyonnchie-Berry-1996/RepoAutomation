b431404a11b77dd37e5d1ba620332df928307f33
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-17 16:35:50 -0500

    Add files via upload

---
+++ git-pull.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change directory to the directory containing your git repos
+cd /mnt/redhatrepos/src
+
+for repo in */; do
+    # Print the name of the repo
+    echo "Found repository: $repo"
+  # Navigate into the repo
+    cd "$repo"
+
+    # Check if it's a git repository
+    if [ -d ".git" ]; then
+        # Fetch updates for the repository
+        echo "Fetching updates for $repo"
+        git pull
+    else
+        echo "$repo is not a git repository"
+    fi
+
+    # Go back to the parent directory
+    cd ..
+
+
+done
+
